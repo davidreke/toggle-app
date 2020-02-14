392b241917cc914b6c5426532951c1c6465e1c01
davidreke <54542172+davidreke@users.noreply.github.com>
2020-02-14 16:13:30 -0500

    Updates

---
+++ precache-manifest.af6b9533bd7dd78b956afa2e54c5ec7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb0d900137f24d7e38b9787b211f694",
+    "url": "/toggle-app/index.html"
+  },
+  {
+    "revision": "9ba7879a8f8f058396ec",
+    "url": "/toggle-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ca5dd191eb07ac0e17ac",
+    "url": "/toggle-app/static/js/2.5a8c08bc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/toggle-app/static/js/2.5a8c08bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba7879a8f8f058396ec",
+    "url": "/toggle-app/static/js/main.0f48682e.chunk.js"
+  },
+  {
+    "revision": "a3dd57d522249f88c3bf",
+    "url": "/toggle-app/static/js/runtime-main.abf993c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/toggle-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
